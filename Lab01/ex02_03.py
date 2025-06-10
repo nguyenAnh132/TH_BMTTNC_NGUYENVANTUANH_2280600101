4551688e96633576d921bc6cce073f8eb6374e0f
nguyenAnh132 <nguyenanhkg123@gmail.com>
2025-06-10 09:15:05 +0700

    [add] Lab04 aes_rsa_socket

--- Lab01/ex02_03.py
+++
@@ -1,5 +0,0 @@
-number = int(input("Nhập một số nguyên: "))
-if number % 2 == 0 :
-    print(number, " là số chẵn")
-else :
-    print(number, " là số lẻ")
--- Lab01/ex02_06.py
+++
@@ -1,7 +0,0 @@
-row = int(input("Nhập vào số dòng: "))
-col = int(input("Nhập vào số cột: "))
-matrix = [[0 for col in range(col)] for row in range(row)]
-for r in range(row):
-    for c in range(col):
-        matrix[r][c] = r* c
-print(matrix)
